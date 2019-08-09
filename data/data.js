6804fe4de6b258e67e09af162873fd0689aa3071
unknown <robertrey1234@gmail.com>
2019-08-09 11:31:34 -0500

    initial commit

---
+++ data/data.js
@@ -0,0 +1,25 @@
+const userArray = [
+    {
+        id: 1,
+        name: Naruto,
+        chores: [
+            {
+                id: 1,
+                desc: "Clean room",
+                completed: false
+            },
+            {
+                id: 2,
+                desc: "Get more ramen",
+                completed: true
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: Shikamaru,
+        chores: [
+            
+        ]
+    }
+]
